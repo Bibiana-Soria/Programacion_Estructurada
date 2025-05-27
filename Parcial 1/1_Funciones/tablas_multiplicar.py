37f276fb6ffa61b6fecf852277493004489b505f
Bibiana-Soria <bibiana_3141240181@utd.edu.mx>
2025-05-27 17:42:00 -0600

    Add files via upload

---
+++ Parcial 1/1_Funciones/tablas_multiplicar.py
@@ -0,0 +1,63 @@
+'''
+Crear un programa que calcule e imprima cualquier tabla de multiplicar
+
+Requisitos:
+1.- Sin estructuras de control
+2.- Sin funciones
+'''
+#version 1
+'''
+num=int(input("Dame el número de la tabla de multiplicar a calcular: "))
+tabla=num*1
+print(f"{num} x 1 = {tabla}")
+tabla=num*2
+print(f"{num} x 2 = {tabla}")
+tabla=num*3
+print(f"{num} x 3 = {tabla}")
+tabla=num*4
+print(f"{num} x 4 = {tabla}")
+tabla=num*5
+print(f"{num}2 x 5 = {tabla}")
+tabla=num*6
+print(f"{num} x 6 = {tabla}")
+tabla=num*7
+print(f"{num} x 7 = {tabla}")
+tabla=num*8
+print(f"{num} x 8 = {tabla}")
+tabla=num*9
+print(f"{num} x 9 = {tabla}")
+tabla=num*10
+print(f"{num} x 10 = {tabla}")
+
+'''
+#version 2
+"""
+for i in range(1,11):
+    tabla=num*i
+    i+=1
+    print(f"{num} x {i} = {tabla}")
+
+    num=int(input("Dame el número de la tabla de multiplicar a calcular: "))
+
+    
+i=1
+while i<=10:
+    tabla=num*i
+    print(f"{num} x {i} = {tabla}")
+    i+=1
+"""
+
+#Version 3
+
+def mult(num):
+    numero=num
+    resp=""
+    for i in range(1,11):
+        tabla=num*i
+        resp=+f"\t{numero} x {i} = {tabla}\n"
+    return resp
+
+num=int(input("Dame el número de la tabla de multiplicar a calcular: "))
+
+
+    
---
+++ Parcial 1/2_modulos_paquetes/paquete1/modulos_paquetes.py
@@ -0,0 +1,3 @@
+# Un modulo es simplemente un archivo con extenión .py que contiene código de Python (funciones, clases, variables, etc.)
+
+#Un paquete es una carpeta que contiene varios módulos (archivos .py) y un archivo especial llamado __init__.py, que le indica a Python que esa carpeta debe tratarseb como un paquete.
