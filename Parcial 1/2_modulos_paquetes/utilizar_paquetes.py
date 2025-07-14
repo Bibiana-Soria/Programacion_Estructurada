bcca3a31e81b387a1f4147c63ed7bc708f0f0b2e
Bibiana-Soria <bibiana_3141240181@utd.edu.mx>
2025-07-14 17:36:23 -0600

    Delete Parcial 1 directory

--- Parcial 1/2_modulos_paquetes/utilizar_paquetes.py
+++
@@ -1,9 +0,0 @@
-from paquete1 import modulos
-
-print(modulos.saludar("Adele"))
-
-modulos.borrarPantalla()
-nom,tel=modulos.solicitarDatos2()
-print(f"\n\t.:: Agenda Telefonica ::.\n\t\tNombre:{nom}\n\t\tTelefono:{tel}")
-modulos.espereTecla()
-
